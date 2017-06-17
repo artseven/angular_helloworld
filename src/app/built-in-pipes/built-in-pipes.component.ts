b322a4c7fc9f2a4569999aff862a95967a7c2175
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-06-17 15:31:23 -0400

    Custom pipe added

--- src/app/built-in-pipes/built-in-pipes.component.ts
+++ src/app/built-in-pipes/built-in-pipes.component.ts
@@ -4,13 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-built-in-pipes',
   template: `
     <p> My name is {{ 'Jack' | uppercase }} </p>
+    <p> Date: {{ today }} </p>
+    <p> Date: {{ today | date:'dd/MM/yyyy' }} </p>
+    <button (click)="increment()">Add one day! </button>
   `,
   styles: []
 })
 export class BuiltInPipesComponent implements OnInit {
-
+  today = new Date()
   constructor() { }
 
   ngOnInit() { }
+
+  increment() {
+    const changedDate = new Date(this.today)
+
+    changedDate.setDate(this.today.getDate() + 1 );
+    this.today = changedDate;
+  }
 
 }
